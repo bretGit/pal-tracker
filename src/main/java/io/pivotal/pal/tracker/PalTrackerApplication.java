2a26308508cd9ed36246c4343b0d2ed1c4955546
pal_user <bret.mcfarland@wellsfargo.com>
2018-05-09 13:21:12 -0600

    Lab changes contd

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -25,7 +25,7 @@ public class PalTrackerApplication {
 
     @Bean
     TimeEntryRepository timeEntryRepository(DataSource datasource) {
-        return new JdbcTimeEntryRepository(dataSource);
+        return new JdbcTimeEntryRepository(datasource);
     }
 
     @Bean
